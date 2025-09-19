63c76980dad8978e79fd6cfc9fa72f62d0bd64e4
gpatigno <adolfo.patino@udea.edu.co>
2025-09-19 09:32:31 -0500

    Uploading files Taller Unidad 2.

---
+++ TALLER_UNIDAD2/PR_CS_2025/PARTE 1/2_producer_consumer_fixed_v1.cpp
@@ -0,0 +1,39 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+int buffer = 0;
+std::mutex mutex;
+
+void produce(const int threadId)
+{
+    std::cout << "Producer started on thread: " << threadId << std::endl;
+    while(1)
+    {
+        mutex.lock();
+        ++buffer;
+        std::cout << buffer << std::endl;
+        mutex.unlock();
+    }
+}
+void consume(const int threadId)
+{
+    std::cout << "Consumer started on thread: " << threadId << std::endl;
+    while(1)
+    {
+        mutex.lock();
+        --buffer;
+        std::cout << buffer << std::endl;
+        mutex.unlock();
+    }
+}
+int main()
+{
+    std::cout << "buffer address: " << &buffer << std::endl;
+    std::thread producer(produce, 0);
+    std::thread consumer(consume, 1);
+    producer.join();
+    consumer.join();
+    return 0;
+}
+
